b6ac7bce845f29c723824c2b205872de29e69f11
Ozgen Koklu <ozgen.koklu@gmail.com>
2023-07-02 19:09:04 +0300

    Added Interfaces
    altered the code to work with interfaces. modified references to clearcounter to IKitchenObjectParent

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -6,32 +6,32 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
-    private ClearCounter clearCounter; 
+    private IKitchenObjectParent kitchenObjectParent; 
     public KitchenObjectSO GetKitchenObjectSO() {
     return kitchenObjectSO;
     }
 
-    public void SetClearCounter(ClearCounter clearCounter) 
+    public void SetKitchenObjectParent(IKitchenObjectParent kitchenObjectParent) 
     {
-        if (this.clearCounter != null)
+        if (this.kitchenObjectParent != null)
         {
-            this.clearCounter.ClearKitchenObject();
+            this.kitchenObjectParent.ClearKitchenObject();
         }
-        //this.clearCounter ve clearCounterýn farklý olduðuna dikkat et.
-        this.clearCounter = clearCounter;
+        //this.clearCounter ve clearCounterýn (IKitchenObjectParent ile deðiþti) farklý olduðuna dikkat et.
+        this.kitchenObjectParent = kitchenObjectParent;
 
-        if(clearCounter.HasKitchenObject()) {
-            Debug.LogError("Counter already has a kitchenObject!");
+        if(kitchenObjectParent.HasKitchenObject()) {
+            Debug.LogError("IKitchenObjectParent already has a kitchenObject!");
         }
-        clearCounter.SetKitchenObject(this);
+        kitchenObjectParent.SetKitchenObject(this);
 
-        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.parent = kitchenObjectParent.GetKitchenObjectFollowTransform();
         transform.localPosition = Vector3.zero;
     }
 
-    public ClearCounter GetClearCounter()
+    public IKitchenObjectParent GetKitchenObjectParent()
     {
-        return clearCounter;
+        return kitchenObjectParent;
     }
 
 }
