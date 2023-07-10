3a3d951a7f53c541995af87b04e6d9832e09c905
Ozgen Koklu <ozgen.koklu@gmail.com>
2023-07-10 13:56:21 +0300

    Added stove counter & logic,
    -Stove Counter prefab from base counter
    -stove counter cs inheriting from basecounter cs
    -FryingRecipe scriptable object to handle stove interactions(made from cuttingRecipeSO)
    -State machine logic in frying states (idle, frying, fried, burned)

---
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static CuttingCounter;
+
+public class StoveCounter : BaseCounter
+{
+
+    public enum State
+    {
+        Idle,
+        Frying,
+        Fried,
+        Burned,
+    }
+
+    [SerializeField] private FryingRecipeSO[] fryingRecipeSOArray;
+
+    private State state;
+    private float fryingTimer;
+    private FryingRecipeSO fryingRecipeSO;
+
+    private void Start()
+    {
+        state = State.Idle; 
+    }
+    private void Update()
+    {
+        if (HasKitchenObject())
+        {
+            switch (state)
+            {
+                case State.Idle:
+                    break;
+                case State.Frying:
+
+                    fryingTimer += Time.deltaTime;
+
+                    if (fryingTimer > fryingRecipeSO.fryingTimerMax)
+                    {
+                        //fried
+                        GetKitchenObject().DestroySelf();
+
+                        KitchenObject.SpawnKitchenObject(fryingRecipeSO.output, this);
+                        state = State.Fried;
+                    }
+                    Debug.Log(fryingTimer);
+            
+                break;
+                case State.Fried:
+                break;
+                case State.Burned:
+                break;
+
+            }
+            Debug.Log(state);
+        }
+       
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!HasKitchenObject())
+        {
+            //there is no kitchenObject here
+            if (player.HasKitchenObject())
+            {
+                //player is carrying something.
+                if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
+                {
+                    //If player has a kitchen object with matching recipe
+                    player.GetKitchenObject().SetKitchenObjectParent(this);  
+                    fryingRecipeSO = GetFryingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+                    state = State.Frying;
+                    fryingTimer = 0f;
+                }
+            }
+            else
+            {
+                //player not caring anything 
+            }
+        }
+        else
+        {
+            //there is a kitchenObject on the counter
+            if (player.HasKitchenObject())
+            {//player is cariyng something
+
+            }
+            else
+            {//player is not cariyng anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
+    }
+
+    private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSOWithInput(inputKitchenObjectSO);
+        return fryingRecipeSO != null;
+    }
+
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSOWithInput(inputKitchenObjectSO);
+        if (fryingRecipeSO != null)
+        {
+            return fryingRecipeSO.output;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private FryingRecipeSO GetFryingRecipeSOWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        foreach (FryingRecipeSO fryingRecipeSO in fryingRecipeSOArray)
+        {
+            if (fryingRecipeSO.input == inputKitchenObjectSO)
+            {
+                return fryingRecipeSO;
+            }
+        }
+        return null;
+    }
+    
+}
